95d7a7ac7330af27c7c7c18eb310033620fc3512
selvam16 <vmselvamaj@gmail.com>
2021-09-03 18:54:44 +0530

    Add files via upload

---
+++ src/components/controls/controls.js
@@ -0,0 +1,11 @@
+import Input from './input'
+import RadioGroup from './radioGroup'
+import Select from './select'
+import Checkbox from './checkbox'
+import DatePicker from './datePicker'
+import Button from './button'
+const Controls = {
+    Input, RadioGroup, Select, Checkbox, DatePicker, Button
+}
+
+export default Controls
