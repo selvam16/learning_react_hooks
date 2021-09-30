aad3847e07932daaac26b5ca09c5a81db4462b5a
selvam16 <vmselvamaj@gmail.com>
2021-09-30 18:39:07 +0530

    Update home.js

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,22 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React from 'react'
+import { useFetch } from '../Components/CustomCooks/useFetchData'
 export default function Home(props) {
 
-    const [customers, setCustomers] = useState([])
+    const {
+        loading, data, error
+    } = useFetch('get', '/customers', {})
 
-    useEffect(() => {
-        axios.get('/customers')
-            .then(res => setCustomers(res.data))
-            .catch(err => console.log(err))
-    }, [])
+    //const [customers, setCustomers] = useState([])
+
+    // useEffect(() => {
+    //     axios.get('/customers')
+    //         .then(res => setCustomers(res.data))
+    //         .catch(err => console.log(err))
+    // }, [])
     return (
-        <>
+        <div style={{ textAlign: 'center' }}>
             <h1>Getting the customers response from proxy</h1>
+            {
+                loading && <span>Loading...</span>
+            }
+            {
+                error && <span style={{ color: 'red' }}>{error}</span>
+            }
             <ul>
                 {
-                    customers.map(c => <li key={c.name}>{c.name}</li>)
+                    data.map(c => <li key={c.name}>{c.name}</li>)
                 }
             </ul>
-        </>
+        </div>
     )
 }
