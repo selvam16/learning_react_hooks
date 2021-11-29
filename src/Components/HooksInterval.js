9948b9f38c265204b94b9dda14f55d4145a30600
selvam16 <vmselvamaj@gmail.com>
2021-11-29 17:23:43 +0530

    Add files via upload

--- src/Components/HooksInterval.js
+++ src/Components/HooksInterval.js
@@ -24,3 +24,41 @@ export default function HooksInterval(props) {
         </>
     )
 }
+
+// with class component
+
+// import React, { Component } from 'react';
+
+// class CounterClass extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       counter: 0,
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.timer = setInterval(() => {
+//       this.setState({
+//         counter: this.state.counter + 1,
+//       });
+//     }, 1000);
+
+//     console.log(this.timer);
+//   }
+
+//   componentWillUnmount() {
+//     console.log('test here', this.timer)
+//     clearInterval(this.timer);
+//   }
+//   render() {
+//     const {
+//       state: { counter },
+//     } = this;
+
+//     return <span>{counter}</span>;
+//   }
+// }
+
+// export default CounterClass;
+
